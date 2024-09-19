16df772f109873390591be201909d38a8317ce3d
MOHAMMAD DANISH <66581682+danish04dz@users.noreply.github.com>
2024-09-20 01:41:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,44 @@
 const express = require("express");
 const app = express();
-
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
-//cookie-parser - what is this and why we need this ?
-
+// Middleware for parsing cookies
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
+// Middleware for parsing JSON
 app.use(express.json());
 
-
+// Database connection
 require("./config/database").connect();
 
-//add client sidde link 
-const cors = require("cors")
-
-
-const allowedOrigins =[
-   // "http://127.0.0.1:5500"
-
+// CORS setup
+const cors = require("cors");
+const allowedOrigins = [
     "https://ctf2024.netlify.app"
 ];
 
-
-// CORS options to handle allowed origins
+// CORS options
 const corsOptions = {
     origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
     },
-    credentials: true,  // Enable cookies and other credentials
-  };
-  
-  // Apply CORS middleware with options
-  app.use(cors(corsOptions));
-  
-  // Middleware to parse JSON
-  app.use(express.json());
-//route import and mount
+    credentials: true, // Allow credentials (like cookies) to be included
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Route import and mount
 const user = require("./routes/user");
 app.use("/api/v1", user);
 
-//actuivate
-
+// Start the server
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`);
-})
+});
